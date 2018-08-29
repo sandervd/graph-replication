e3fcc5019672fe8b6c097575185c3b2695e73c8b
Sander Van Dooren <sander.vandooren@gmail.com>
2018-08-29 14:11:16 +0200

    Build the triplestore state from kafka transactions.

--- src/main/java/eu/europa/ec/contentlayer/pod/App.java
+++ src/main/java/eu/europa/ec/contentlayer/pod/App.java
@@ -1,12 +1,20 @@
 package eu.europa.ec.contentlayer.pod;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 import eu.europa.ec.contentlayer.pod.pojo.RdfTransaction;
+import org.apache.jena.atlas.io.IndentedWriter;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.rdfconnection.RDFConnectionFuseki;
+import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
+import org.apache.jena.sparql.modify.request.UpdateWriter;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateRequest;
 import org.apache.jena.util.FileManager;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -25,8 +33,44 @@ public class App {
 		runConsumer();
 	}
 
+	/**
+	 * Split a model in a list of models per subject.
+	 *
+	 * @param model The large model, containing multiple entities.
+	 * @return Sub-models per entity.
+	 */
+	private static Map<String, Model> extractSubjectModels(Model model) {
+		Map<String, Model> ExtractedModels = new HashMap<>();
+		ResIterator AllResources = model.listSubjects();
+		while (AllResources.hasNext()) {
+			Resource ResourceToProcess = AllResources.nextResource();
+			if (ResourceToProcess.isAnon()) {
+				continue;
+			}
+			List<Statement> ResourceProperties = ResourceToProcess.listProperties().toList();
+			Queue<Statement> StatementQueue = new LinkedList<>(ResourceProperties);
+			Model ItemModel = ModelFactory.createDefaultModel();
+			// 'recursively' fetch blank node statements.
+			while (StatementQueue.size() > 0) {
+				Statement StatementToProcess = StatementQueue.remove();
+				if (StatementToProcess.getObject().isAnon()) {
+					Resource subResource = StatementToProcess.getObject().asResource();
+					List<Statement> SubResourceProperties = subResource.listProperties().toList();
+					StatementQueue.addAll(SubResourceProperties);
+				}
+				ItemModel.add(StatementToProcess);
+			}
+			ExtractedModels.put(ResourceToProcess.getURI(), ItemModel);
+		}
+		return ExtractedModels;
+	}
+
 	static void runConsumer() {
 		Consumer<String, RdfTransaction> consumer = ConsumerCreator.createConsumer();
+
+        RDFConnectionRemoteBuilder builder = RDFConnectionFuseki.create()
+                .destination("http://localhost:3030/datapod/query")
+                .updateEndpoint("http://localhost:3030/datapod/update");
 
 		int noMessageToFetch = 0;
 
@@ -41,7 +85,33 @@ public class App {
 			}
 
 			consumerRecords.forEach(record -> {
-				System.out.println("Record Key " + record.key());
+				Model model = ModelFactory.createDefaultModel();
+                String serializedStatements = record.value().getStatements();
+
+                InputStream stream = new ByteArrayInputStream(serializedStatements.getBytes(StandardCharsets.UTF_8));
+
+				// read the RDF/XML file
+				model.read(stream, null);
+
+                OutputStream outStream = new ByteArrayOutputStream();
+                IndentedWriter writeout = new IndentedWriter(outStream);
+                UpdateWriter updateWriter = new UpdateWriter(writeout, null);
+                updateWriter.open();
+
+				StmtIterator Triples = model.listStatements();
+				while (Triples.hasNext()) {
+				    Statement Triple = Triples.nextStatement();
+				    String URI = "http://" + record.topic() + "/";
+                    Node graph = NodeFactory.createURI(URI);
+                    updateWriter.insert(graph, Triple.asTriple());
+                }
+
+				updateWriter.close();
+				writeout.flush();
+                writeout.close();
+                String insert = (outStream).toString();
+                UpdateRequest query = UpdateFactory.create(insert);
+                builder.build().update(query);
 			});
 			consumer.commitAsync();
 
@@ -54,24 +124,23 @@ public class App {
 
 		Model model = getModel();
 
-		ResIterator Subjects = model.listSubjects();
-		while (Subjects.hasNext()) {
-			Resource subject = Subjects.nextResource();
-			RdfTransaction Transaction = buildRdfTransaction(subject);
-			commitTransaction(producer, Transaction, subject.toString());
-		}
+		Map<String, Model> subjectModels = extractSubjectModels(model);
+		subjectModels.forEach((subject, subjectModel) -> {
+			System.out.println(serializeModel(subjectModel));
+			RdfTransaction Transaction = buildRdfTransaction(subjectModel);
+			commitTransaction(producer, Transaction, subject);
+		});
 	}
 
-	private static RdfTransaction buildRdfTransaction(Resource subject) {
+	private static RdfTransaction buildRdfTransaction(Model model) {
 		RdfTransaction Transaction = new RdfTransaction();
-
-		Transaction.setStatements(serializeStatements(subject));
-
+		Transaction.setStatements(serializeModel(model));
 		return Transaction;
 	}
 
 	private static Model getModel() {
-		String inputFileName = "/home/sander/eurovoc.rdf";
+		String inputFileName = "/home/sander/countries-skos.rdf";
+		// String inputFileName = "/home/sander/eurovoc.rdf";
 
 		// create an empty model
 		Model model = ModelFactory.createDefaultModel();
@@ -88,11 +157,9 @@ public class App {
 		return model;
 	}
 
-	private static String serializeStatements(Resource subject) {
-		StmtIterator Properties = subject.listProperties();
+	private static String serializeModel(Model model) {
 		OutputStream out = new ByteArrayOutputStream();
-		Properties.toModel().write(out);
-
+		model.write(out);
 		return out.toString();
 	}
 
