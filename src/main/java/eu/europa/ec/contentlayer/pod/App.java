e8a01069d3cdfb78e35aa0b09bea2a7f3b4b8211
Sander Van Dooren <sander.vandooren@gmail.com>
2018-08-30 09:35:56 +0200

    Cleanup, extract some methods.

--- src/main/java/eu/europa/ec/contentlayer/pod/App.java
+++ src/main/java/eu/europa/ec/contentlayer/pod/App.java
@@ -17,6 +17,7 @@ import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateRequest;
 import org.apache.jena.util.FileManager;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -30,11 +31,13 @@ import eu.europa.ec.contentlayer.pod.producer.ProducerCreator;
 public class App {
 	public static void main(String[] args) {
 		runProducer();
+		System.out.println("Data send");
 		runConsumer();
+		System.out.println("Data consumed");
 	}
 
 	/**
-	 * Split a model in a list of models per subject.
+	 * Split a model in a list of models per subject (subject models).
 	 *
 	 * @param model The large model, containing multiple entities.
 	 * @return Sub-models per entity.
@@ -47,28 +50,40 @@ public class App {
 			if (ResourceToProcess.isAnon()) {
 				continue;
 			}
-			List<Statement> ResourceProperties = ResourceToProcess.listProperties().toList();
-			Queue<Statement> StatementQueue = new LinkedList<>(ResourceProperties);
-			Model ItemModel = ModelFactory.createDefaultModel();
-			// 'recursively' fetch blank node statements.
-			while (StatementQueue.size() > 0) {
-				Statement StatementToProcess = StatementQueue.remove();
-				if (StatementToProcess.getObject().isAnon()) {
-					Resource subResource = StatementToProcess.getObject().asResource();
-					List<Statement> SubResourceProperties = subResource.listProperties().toList();
-					StatementQueue.addAll(SubResourceProperties);
-				}
-				ItemModel.add(StatementToProcess);
-			}
+			Model ItemModel = expandBlankNodes(ResourceToProcess);
 			ExtractedModels.put(ResourceToProcess.getURI(), ItemModel);
 		}
 		return ExtractedModels;
 	}
 
+	/**
+	 * Expand a resource into a subject model; containing all properties related to the subject.
+	 *
+	 * @param ResourceToProcess
+	 * 	The resource object of the subject to expand.
+	 * @return
+	 *  The subject model.
+	 */
+	private static Model expandBlankNodes(Resource ResourceToProcess) {
+		List<Statement> ResourceProperties = ResourceToProcess.listProperties().toList();
+		Queue<Statement> StatementQueue = new LinkedList<>(ResourceProperties);
+		Model ItemModel = ModelFactory.createDefaultModel();
+		while (StatementQueue.size() > 0) {
+			Statement StatementToProcess = StatementQueue.remove();
+			if (StatementToProcess.getObject().isAnon()) {
+				Resource subResource = StatementToProcess.getObject().asResource();
+				List<Statement> SubResourceProperties = subResource.listProperties().toList();
+				StatementQueue.addAll(SubResourceProperties);
+			}
+			ItemModel.add(StatementToProcess);
+		}
+		return ItemModel;
+	}
+
 	static void runConsumer() {
 		Consumer<String, RdfTransaction> consumer = ConsumerCreator.createConsumer();
 
-        RDFConnectionRemoteBuilder builder = RDFConnectionFuseki.create()
+        RDFConnectionRemoteBuilder tripleStoreConnection = RDFConnectionFuseki.create()
                 .destination("http://localhost:3030/datapod/query")
                 .updateEndpoint("http://localhost:3030/datapod/update");
 
@@ -80,53 +95,73 @@ public class App {
 				noMessageToFetch++;
 				if (noMessageToFetch > IKafkaConstants.MAX_NO_MESSAGE_FOUND_COUNT)
 					break;
-				else
-					continue;
 			}
 
 			consumerRecords.forEach(record -> {
-				Model model = ModelFactory.createDefaultModel();
-                String serializedStatements = record.value().getStatements();
-
-                InputStream stream = new ByteArrayInputStream(serializedStatements.getBytes(StandardCharsets.UTF_8));
-
-				// read the RDF/XML file
-				model.read(stream, null);
-
-                OutputStream outStream = new ByteArrayOutputStream();
-                IndentedWriter writeout = new IndentedWriter(outStream);
-                UpdateWriter updateWriter = new UpdateWriter(writeout, null);
-                updateWriter.open();
-
-				StmtIterator Triples = model.listStatements();
-				while (Triples.hasNext()) {
-				    Statement Triple = Triples.nextStatement();
-				    String URI = "http://" + record.topic() + "/";
-                    Node graph = NodeFactory.createURI(URI);
-                    updateWriter.insert(graph, Triple.asTriple());
-                }
-
-				updateWriter.close();
-				writeout.flush();
-                writeout.close();
-                String insert = (outStream).toString();
-                UpdateRequest query = UpdateFactory.create(insert);
-                builder.build().update(query);
+				Model model = transactionToSubjectModel(record);
+				String graphName = "http://" + record.topic() + "/";
+				String insert = subjectModelToInsertQuery(model, graphName);
+				UpdateRequest query = UpdateFactory.create(insert);
+				try {
+					tripleStoreConnection.build().update(query);
+				}
+				catch (Exception e) {
+					System.out.println("Error inserting triples");
+				}
 			});
 			consumer.commitAsync();
-
 		}
 		consumer.close();
+	}
+
+	/**
+	 * Transform a model into an SPARQL insert query.
+	 *
+	 * @param model
+	 *  The subject model.
+	 * @param graphName
+	 *   The name of the graph where to insert the triples.
+	 * @return
+	 *   The formatted insert query.
+	 */
+	private static String subjectModelToInsertQuery(Model model, String graphName) {
+		// @todo Can we avoid all the stream wrapper madness?
+		OutputStream outStream = new ByteArrayOutputStream();
+		IndentedWriter writeout = new IndentedWriter(outStream);
+		UpdateWriter updateWriter = new UpdateWriter(writeout, null);
+		updateWriter.open();
+
+		StmtIterator Triples = model.listStatements();
+		while (Triples.hasNext()) {
+			Statement Triple = Triples.nextStatement();
+			Node graph = NodeFactory.createURI(graphName);
+			updateWriter.insert(graph, Triple.asTriple());
+		}
+
+		updateWriter.close();
+		writeout.flush();
+		writeout.close();
+		return (outStream).toString();
+	}
+
+	private static Model transactionToSubjectModel(ConsumerRecord<String, RdfTransaction> record) {
+		Model model = ModelFactory.createDefaultModel();
+		String serializedStatements = record.value().getStatements();
+
+		InputStream stream = new ByteArrayInputStream(serializedStatements.getBytes(StandardCharsets.UTF_8));
+
+		// read the RDF/XML file
+		model.read(stream, null);
+		return model;
 	}
 
 	static void runProducer() {
 		Producer<String, RdfTransaction> producer = ProducerCreator.createProducer();
 
-		Model model = getModel();
+		Model model = getFileBasedModel();
 
 		Map<String, Model> subjectModels = extractSubjectModels(model);
 		subjectModels.forEach((subject, subjectModel) -> {
-			System.out.println(serializeModel(subjectModel));
 			RdfTransaction Transaction = buildRdfTransaction(subjectModel);
 			commitTransaction(producer, Transaction, subject);
 		});
@@ -138,7 +173,7 @@ public class App {
 		return Transaction;
 	}
 
-	private static Model getModel() {
+	private static Model getFileBasedModel() {
 		String inputFileName = "/home/sander/countries-skos.rdf";
 		// String inputFileName = "/home/sander/eurovoc.rdf";
 
@@ -167,8 +202,8 @@ public class App {
 		final ProducerRecord<String, RdfTransaction> record = new ProducerRecord<>(IKafkaConstants.TOPIC_NAME, null, Subject, transaction);
 		try {
 			RecordMetadata metadata = producer.send(record).get();
-			System.out.println("Record sent to partition " + metadata.partition()
-					+ " with offset " + metadata.offset());
+			// System.out.println("Record sent to partition " + metadata.partition()
+			//		+ " with offset " + metadata.offset());
 		}
 		// @todo Rethrow exception to cancel triplestore transaction.
 		catch (ExecutionException |InterruptedException e) {
